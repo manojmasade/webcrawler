bdaa5a21c18da7260fc716a9157e9399ad864f4b
manojmasade <manojmasade@gmail.com>
2015-02-11 18:10:11 +0530

    Added CrawlerApp with project skeleton

---
+++ src/main/java/com/imaginea/training/crawler/App.java
@@ -0,0 +1,13 @@
+package com.imaginea.training.crawler;
+
+/**
+ * Java Crawler
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Java Crawler - Download emails for year 2014" );
+    }
+}
