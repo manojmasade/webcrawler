ac9d5b5f30994aef8087f2781758da41bf97c01d
manojmasade <manojmasade@gmail.com>
2015-02-12 12:25:07 +0530

    hyphen added

--- src/main/java/com/imaginea/training/crawler/App.java
+++ src/main/java/com/imaginea/training/crawler/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Java Crawler - Download emails for year 2014" );
+        System.out.println( "Java Crawler -- Download emails for year 2014" );
     }
 }
