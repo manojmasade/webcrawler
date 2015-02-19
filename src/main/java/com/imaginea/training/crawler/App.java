931bdb2ff66dbd1d1d76e475768e44d8a15b364a
manojmasade <manojmasade@gmail.com>
2015-02-19 18:16:35 +0530

    integrated with Crawler

--- src/main/java/com/imaginea/training/crawler/App.java
+++ src/main/java/com/imaginea/training/crawler/App.java
@@ -1,13 +1,22 @@
 package com.imaginea.training.crawler;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.imaginea.training.crawler.core.Crawler;
+
 /**
- * Java Crawler
+ * App: Java Crawler
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Java Crawler -- Download emails for year 2014" );
+public class App {
+	private static final Logger logger = LoggerFactory.getLogger(App.class);
+	
+    public static void main( String[] args ) {
+    	logger.info("Java Crawler -- Download emails for year 2014" );
+        
+        Crawler crawler = new Crawler();
+        crawler.init();
+        crawler.start();
     }
 }
