bbb14260c7be3ca997bb213f117c7b10f1e0e6b8
manojmasade <manojmasade@gmail.com>
2015-02-26 16:16:31 +0530

    commented method removed.

--- src/main/java/com/imaginea/training/crawler/util/NetUtil.java
+++ src/main/java/com/imaginea/training/crawler/util/NetUtil.java
@@ -1,37 +1,16 @@
 package com.imaginea.training.crawler.util;
 
 import java.io.IOException;
-import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.imaginea.training.crawler.constant.Constant;
-
 public class NetUtil implements INetUtil {
 	
 	private static final Logger logger = LoggerFactory.getLogger(NetUtil.class);
 
-	/*@Override
-	public Boolean isInternetReachable() {
-		try {
-			InetAddress address = InetAddress.getByName("java.sun.com");
-			logger.info("address:" + address);
-			if(address == null){
-				return false;
-			}
-		} catch (UnknownHostException e) {
-			//logger.info("Connection not reachable to host, {}", e);
-			return false;
-		} catch (Exception e) {
-			//logger.info("Unexpected exception in obtaining connection, {}", e);
-			return false;
-		}
-		return true;
-	}*/
-	
 	@Override
 	public Boolean isInternetReachable() {
 		Socket socket = null;
@@ -44,7 +23,7 @@ public class NetUtil implements INetUtil {
 		} finally {            
 		    if (socket != null) try { socket.close(); } catch(IOException e) {}
 		}
-		//logger.info("reachable:" + reachable);
+		logger.debug("isInternetReachable:" + reachable);
 		return reachable;
 	}
 
