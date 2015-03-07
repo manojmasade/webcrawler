b3ad92a549478c0be8c9ee39d7db08a98425d468
manojmasade <manojmasade@gmail.com>
2015-03-07 22:42:22 +0530

    author tag added

--- src/main/java/com/imaginea/training/crawler/core/AbstractCrawler.java
+++ src/main/java/com/imaginea/training/crawler/core/AbstractCrawler.java
@@ -6,6 +6,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
+/**
+ * 
+ * @author manojm
+ *
+ */
 public abstract class AbstractCrawler {
 	
 	private static final Logger logger = LoggerFactory.getLogger(AbstractCrawler.class);
